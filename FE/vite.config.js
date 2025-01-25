f9dc7f1796d684e7f43243ca9c9d0e85fe871137
huetruong <truongvshieu01@gmail.com>
2025-01-25 09:30:26 +0700

    update

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -30,4 +31,5 @@ export default defineConfig({
       },
     },
   },
-})
+  base: './', // Add this line
+});
