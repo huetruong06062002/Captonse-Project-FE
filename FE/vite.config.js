2b5a0c9cb1ed7a7da391c074a2b4c8d33c040038
huetruong <truongvshieu01@gmail.com>
2025-01-25 09:28:05 +0700

    update

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -21,4 +21,13 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 4173,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
