2e87ddc4c3817db8de9f4bcbb17612636bb3018d
huetruong <truongvshieu01@gmail.com>
2025-03-22 23:45:19 +0700

    update

--- StaffApp/App.jsx
+++ StaffApp/App.jsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigator } from "./src/navigation/Navigation";
 import { Provider } from "react-redux";
 import { store } from './src/redux/store';
+import "react-native-gesture-handler";
 
 export default function App() {
   return (
