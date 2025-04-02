53964c7c304befc8c8407e8114a8f5a7f8554aa1
huetruong <truongvshieu01@gmail.com>
2025-04-02 17:29:46 +0700

    update dashboard

--- FE/src/pages/dashboard/index.jsx
+++ FE/src/pages/dashboard/index.jsx
@@ -16,7 +16,10 @@ import { Col, Image, Row } from "antd";
 
 import { FaUserTie } from "react-icons/fa6";
 import orderImage from "../../assets/image/order.png";
-
+import customerImage from "../../assets/image/customer.png";
+import serviesImage from "../../assets/image/service.png";
+import extraImage from "../../assets/image/services-extra.png";
+import CountUp from "react-countup";
 // Đăng ký các thành phần cần thiết
 ChartJS.register(
   CategoryScale,
@@ -32,6 +35,8 @@ function DashBoard() {
   //fetch api customer
   const [numberOfCustomer, setNumberOfCustomer] = useState(0);
   const [numberOfOrder, setNumberOfOrder] = useState(0);
+  const [numberOfService, setNumberOfService] = useState(0);
+  const [numberOfExtra, setNumberOfExtra] = useState(0);
 
   useEffect(() => {
     const fetchNumberOfCustomer = async () => {
@@ -53,11 +58,34 @@ function DashBoard() {
         console.error("Error fetching users:", error);
       }
     };
+
+    const fetchNumberOfServices = async () => {
+      try {
+        const response = await getRequest("DashBoard/get-all-services-numbers");
+        console.log("response", response);
+        setNumberOfService(response.data.servicesNumbers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    const fetchNumberOfExtras = async () => {
+      try {
+        const response = await getRequest("DashBoard/get-all-extras-numbers");
+        console.log("response", response);
+        setNumberOfExtra(response.data.extrasNumbers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
     fetchNumberOfCustomer();
     fetchNumberOfOrders();
-  }, [numberOfCustomer]);
+    fetchNumberOfServices();
+    fetchNumberOfExtras();
+  }, []);
 
-  console.log("numberOfCustomer", numberOfCustomer);
+  console.log("numberOfExtra", numberOfExtra);
 
   const [orderStats, setOrderStats] = useState(null);
 
@@ -132,7 +160,16 @@ function DashBoard() {
               padding: "1rem",
             }}
           >
-            <FaUserTie style={{ fontSize: "3rem", color: "white" }} />
+            <img
+              src={customerImage}
+              alt="Order"
+              style={{
+                width: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                height: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                objectFit: "contain", // Giữ tỷ lệ hình ảnh
+                borderRadius: "50%", // Bo tròn hình ảnh
+              }}
+            />
           </div>
           <div style={{ marginLeft: "1rem", marginTop: "1rem" }}>
             <p
@@ -147,7 +184,13 @@ function DashBoard() {
             <h3
               style={{ textAlign: "center", color: "#000", fontSize: "1.1rem" }}
             >
-              {numberOfCustomer}
+              {/* Sử dụng CountUp để tạo hiệu ứng tăng số */}
+              <CountUp
+                start={0} // Bắt đầu từ 0
+                end={numberOfCustomer} // Kết thúc tại giá trị thực tế
+                duration={2} // Thời gian hiệu ứng (2 giây)
+                separator="," // Thêm dấu phẩy phân cách số
+              />
             </h3>
           </div>
         </Col>
@@ -181,11 +224,12 @@ function DashBoard() {
                 height: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
                 objectFit: "contain", // Giữ tỷ lệ hình ảnh
                 borderRadius: "50%", // Bo tròn hình ảnh
-
               }}
             />
           </div>
-          <div style={{ marginLeft: "1rem", marginTop: "1rem" , color: "#f1c40f"}}>
+          <div
+            style={{ marginLeft: "1rem", marginTop: "1rem", color: "#f1c40f" }}
+          >
             <p
               style={{
                 textAlign: "center",
@@ -198,32 +242,154 @@ function DashBoard() {
             <h3
               style={{ textAlign: "center", color: "#000", fontSize: "1.1rem" }}
             >
-              { numberOfOrder}
+              <CountUp
+                start={0} // Bắt đầu từ 0
+                end={numberOfOrder} // Kết thúc tại giá trị thực tế
+                duration={2} // Thời gian hiệu ứng (2 giây)
+                separator="," // Thêm dấu phẩy phân cách số
+              />
+            </h3>
+          </div>
+        </Col>
+        <Col
+          span={6}
+          style={{
+            borderRadius: "1rem",
+            backgroundColor: "#f0f0f0",
+            padding: "1rem",
+            display: "flex",
+            color: "#2980b9",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#2ecc71",
+              borderRadius: "50%",
+              width: "5rem",
+              height: "5rem",
+              padding: "1rem",
+              display: "flex", // Sử dụng flexbox
+              justifyContent: "center", // Căn giữa theo chiều ngang
+              alignItems: "center", // Căn giữa theo chiều dọc
+            }}
+          >
+            <img
+              src={serviesImage}
+              alt="Serives"
+              style={{
+                width: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                height: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                objectFit: "contain", // Giữ tỷ lệ hình ảnh
+                borderRadius: "50%", // Bo tròn hình ảnh
+              }}
+            />
+          </div>
+          <div
+            style={{ marginLeft: "1rem", marginTop: "1rem", color: "#f1c40f" }}
+          >
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#2ecc71",
+              }}
+            >
+              Tổng số lượng dịch vụ
+            </p>
+            <h3
+              style={{ textAlign: "center", color: "#000", fontSize: "1.1rem" }}
+            >
+             <CountUp
+                start={0} // Bắt đầu từ 0
+                end={numberOfService} // Kết thúc tại giá trị thực tế
+                duration={2} // Thời gian hiệu ứng (2 giây)
+                separator="," // Thêm dấu phẩy phân cách số
+              />
+            </h3>
+          </div>
+        </Col>
+        <Col
+          span={6}
+          style={{
+            borderRadius: "1rem",
+            backgroundColor: "#f0f0f0",
+            padding: "1rem",
+            display: "flex",
+            color: "#2980b9",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#f1c40f",
+              borderRadius: "50%",
+              width: "5rem",
+              height: "5rem",
+              padding: "1rem",
+              display: "flex", // Sử dụng flexbox
+              justifyContent: "center", // Căn giữa theo chiều ngang
+              alignItems: "center", // Căn giữa theo chiều dọc
+              backgroundColor: "#8e44ad",
+            }}
+          >
+            <img
+              src={extraImage}
+              alt="Order"
+              style={{
+                width: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                height: "100%", // Đảm bảo hình ảnh chiếm toàn bộ không gian div
+                objectFit: "contain", // Giữ tỷ lệ hình ảnh
+                borderRadius: "50%", // Bo tròn hình ảnh
+                backgroundolor: "#8e44ad",
+              }}
+            />
+          </div>
+          <div style={{ marginLeft: "1rem", marginTop: "1rem" }}>
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                color: "#8e44ad",
+              }}
+            >
+              Tổng số dịch vụ thêm
+            </p>
+            <h3
+              style={{ textAlign: "center", color: "#000", fontSize: "1.1rem" }}
+            >
+             <CountUp
+                start={0} // Bắt đầu từ 0
+                end={numberOfExtra} // Kết thúc tại giá trị thực tế
+                duration={2} // Thời gian hiệu ứng (2 giây)
+                separator="," // Thêm dấu phẩy phân cách số
+              />
             </h3>
           </div>
         </Col>
       </Row>
-      <div style={{ width: "25%", float: "left" }}>
-        <h3>Order Statistics</h3>
-        <Pie
-          data={pieData}
-          options={{
-            responsive: true,
-            plugins: { legend: { position: "top" } },
-          }}
-        />
-      </div>
-
-      <div style={{ width: "50%", float: "left" }}>
-        <h3>Số lượng đơn hàng</h3>
-        <Bar
-          data={barData}
-          options={{
-            responsive: true,
-            plugins: { legend: { position: "top" } },
-          }}
-        />
-      </div>
+      <Row style={{ marginTop: "3rem", display: "flex" }}>
+        <div style={{ width: "25%", float: "left" }}>
+          <h3>Order Statistics</h3>
+          <Pie
+            data={pieData}
+            options={{
+              responsive: true,
+              plugins: { legend: { position: "top" } },
+            }}
+          />
+        </div>
+        <div style={{ width: "50%", float: "left" }}>
+          <h3>Số lượng đơn hàng</h3>
+          <Bar
+            data={barData}
+            options={{
+              responsive: true,
+              plugins: { legend: { position: "top" } },
+            }}
+          />
+        </div>
+      </Row>
     </div>
   );
 }
